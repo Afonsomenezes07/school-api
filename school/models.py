36c191c6c94f7a447cedf4e85431a66919726642
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-05-29 19:47:22 -0300

    feat: create student subject and grade models

--- school/models.py
+++ school/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.student} - {self.subject}"
+
 # Create your models here.
