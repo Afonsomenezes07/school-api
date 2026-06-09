6ab468bd9bb7fb79468638dbeb8a9e6638e3d3fe
Afonso Menezes <afonsomenezes2000@gmail.com>
2026-06-08 21:08:59 -0300

    feat: add API pagination

--- core/settings.py
+++ core/settings.py
@@ -105,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 5
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
